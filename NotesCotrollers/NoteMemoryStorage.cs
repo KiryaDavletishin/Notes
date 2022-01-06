5965e1ae0bc55b01a399f7085fa4708a31e48fcf
Dmirty12 <96833795+Dmirty12@users.noreply.github.com>
2022-01-06 21:20:53 +0300

    Second commit

---
+++ NotesCotrollers/NoteMemoryStorage.cs
@@ -0,0 +1,64 @@
+﻿using Notes.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Notes.Controllers
+{
+    public class NoteMemoryStorage : ObservableCollection<INote>
+    {
+        
+        public void Add(string path)
+        {
+            var note = new Note(path);
+
+            Add(note);
+        }
+        public void AddRange(string[] paths)
+        {
+            foreach(var path in paths)
+            {
+                Add(path);
+            }
+        }
+        public bool IsMatchFilter(INote note, string filter)
+        {
+            string pattern = $@"\w*{Regex.Escape(filter)}\w*";
+            var regex = new Regex(pattern);
+            return regex.IsMatch(note.Name);
+        }
+
+        public void RemoveAll()
+        {
+            while (Count != 0)
+                Remove(this.Last());
+        }
+
+        public void SaveNew(INote note, string newPath)
+        {
+            note.Name = newPath;
+            note.Save();
+        }
+        public void Save(INote note)
+        {
+            note.Save();
+        }
+        protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            if(e.Action == NotifyCollectionChangedAction.Remove)
+            {
+                for (int i = 0; i < e.OldItems.Count; i++)
+                {
+                    var deleted = e.OldItems[i] as INote;
+                    deleted.Close();
+                }
+            }
+
+            base.OnCollectionChanged(e);
+        }
+    }
+}
